3a53c9a443288409cc0d78c47562cb5f918b16e1
fout4 <fernando.out4@gmail.com>
2025-07-07 00:45:39 -0300

    Ajustando portugues

--- src/server.js
+++ src/server.js
@@ -15,16 +15,8 @@ server.use('/public', publicRoutes) //rotas na porta 8000
 
 server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddleware);
 
-server.use(router) //rotas na prota 3000
+server.use(router) // TODAS as rotas na porta 3000
 
 server.listen(8000, () => {
-<<<<<<< HEAD
-  console.log("Boas-vindas a API do AllBooks.")
-  console.log("API disponível na URL http://localhost:8000")
-=======
   console.log("Acesse essa API em http://localhost:8000")
->>>>>>> 982ea0b2b3137ba43ba3d6f5722f85a3ce9d0d04
 })
-
-// TESTANDO
-
